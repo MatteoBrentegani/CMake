3fe9e1474193dae3ef5ad62b974c27dbb9c69d8c
MatteoBrentegani <m.brentegani93@live.com>
2023-10-28 12:57:28 +0200

    add make dependency to generate a graph of dependencies

--- HelloWorld/cmake/AddGitSubmodule.cmake
+++ HelloWorld/cmake/AddGitSubmodule.cmake
@@ -1,14 +1,19 @@
 function(add_git_submodule directory)
     find_package(Git REQUIRED)
-
-    if (NOT EXISTS ${directory}/CMakeLists.txt)
-        MESSAGE(STATUS "${GIT_EXECUTABLE} submodule update --init --recursive -- ${directory} ${PROJECT_SOURCE_DIR}/../")
-        execute_process(COMMAND "${GIT_EXECUTABLE} submodule update --init --recursive -- ${directory} "
+    message(STATUS ${CMAKE_SOURCE_DIR})
+    if (NOT EXISTS ${CMAKE_SOURCE_DIR}/${directory}/CMakeLists.txt)
+        MESSAGE(STATUS "${GIT_EXECUTABLE} submodule update --init --recursive -- ${CMAKE_SOURCE_DIR}/${directory} ${PROJECT_SOURCE_DIR}/../")
+        execute_process(COMMAND "${GIT_EXECUTABLE} submodule update --init --recursive -- ${CMAKE_SOURCE_DIR}/${directory} "
             WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}/../
         )
     endif()
-    
-    add_subdirectory(../${directory})
+
+    if (EXISTS ${CMAKE_SOURCE_DIR}/${directory}/CMakeLists.txt)
+        message("Adding: ${directory}/CMakeLists.txt")
+        add_subdirectory(${CMAKE_SOURCE_DIR}/${directory})
+    else()
+        message("Could not add: ${CMAKE_SOURCE_DIR}/${directory}/CMakeLists.txt")
+    endif()
 
 endfunction(add_git_submodule directory)
 
