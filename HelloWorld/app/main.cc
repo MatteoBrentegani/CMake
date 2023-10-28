b952f365dfb69aca6a596a5bcec9d32290b627ab
MatteoBrentegani <m.brentegani93@live.com>
2023-10-28 14:42:45 +0200

    add unit test factorial

--- HelloWorld/app/main.cc
+++ HelloWorld/app/main.cc
@@ -1,5 +1,9 @@
 #include <iostream>
+
+#include <cxxopts.hpp>
 #include <nlohmann/json.hpp>
+#include <fmt/format.h>
+#include <spdlog/spdlog.h>
 
 #include "my_lib.h"
 #include "config.hpp"
@@ -13,7 +17,16 @@ int main()
         << NLOHMANN_JSON_VERSION_MAJOR << "."
         << NLOHMANN_JSON_VERSION_MINOR << "."
         << NLOHMANN_JSON_VERSION_PATCH << "\n";
-
+    std::cout << "FMT Lib version:" 
+        << FMT_VERSION << "\n";    
+    std::cout << "SPDLOG Lib version:" 
+        << SPDLOG_VER_MAJOR << "."
+        << SPDLOG_VER_MINOR << "."
+        << SPDLOG_VER_PATCH << "\n";    
+    std::cout << "CXXOPTS Lib version:" 
+        << CXXOPTS__VERSION_MAJOR << "."
+        << CXXOPTS__VERSION_MINOR << "."
+        << CXXOPTS__VERSION_PATCH << "\n";
     print_hello_world();
 
     return 0;
--- HelloWorld/src/my_lib.cc
+++ HelloWorld/src/my_lib.cc
@@ -1,8 +1,18 @@
 #include <iostream>
+#include <nlohmann/json.hpp>
 
 #include "my_lib.h"
 
+
 void print_hello_world() 
 {
-    std::cout << "Helloo World\n";
+    std::cout << "Helloo World\n";    
+    std::cout << "JSON Lib version:" 
+        << NLOHMANN_JSON_VERSION_MAJOR << "."
+        << NLOHMANN_JSON_VERSION_MINOR << "."
+        << NLOHMANN_JSON_VERSION_PATCH << "\n";
+}
+
+std::uint32_t factorial(std::uint32_t number) {
+    return number <= 1 ? 1 : factorial(number-1)*number;
 }
